ff28caa6d1a85b0cde2f5e53b71dc3e010f313e5
Irving Delgado <rvngdelgado@gmail.com>
2022-02-18 17:58:31 -0600

    completed tasks, character render from data pulled from api, applied styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from "./components/Character"
+
 
 const App = () => {
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+    .then(res => {
+      setCharacters(res.data)
+      
+    }).catch(err => {
+      console.log(err)
+    }, [])
+  })
+
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,7 +28,12 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {
+        characters.map((chr, index)=> {
+          return <Character key= {index} character={chr} />
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,31 @@
 // Write your Character component here
+import React from 'react';
+import styled from "styled-components"
+
+const StyledCharacter = styled.div`
+background-color: white;
+color: purple;
+opacity: 0.4;
+display: flex;
+flex-direction: column;
+justified-content: center;
+margin: 20px;
+
+
+&&:hover {
+    opacity: 0.8;
+}
+`
+
+const Character = (props) => {
+    return (
+        <StyledCharacter>
+            <h2>Name: {props.character.name} </h2>
+            <p>Height: {props.character.height}</p>
+            <p>Hair color: {props.character.hair_color}</p>
+            <p>Skin Color: {props.character.skin_color}</p>
+        </StyledCharacter>
+    )
+}
+
+export default Character;
